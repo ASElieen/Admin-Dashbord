817510fa50bdda75d37e4be26de7cea37556c201
username <790768275@qq.com>
2022-07-07 16:32:11 +0800

    期末 做完了一些小组件部分

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from '../components/Header'
+import {Calendar} from 'antd'
 
-const Calendar = () => {
+const CalendarPage = () => {
   return (
-    <div>Calendar</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='App' title='Calendar' />
+      <Calendar/>
+    </div>
   )
 }
 
-export default Calendar
+export default CalendarPage
